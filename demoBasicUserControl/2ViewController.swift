9a2ecfd3b2886fc8fa347d6574780129f6499c5f
QuanNH97 <quannh220597@gmail.com>
2020-08-11 14:47:38 +0700

    first commit

---
+++ demoBasicUserControl/2ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  2ViewController.swift
+//  demoBasicUserControl
+//
+//  Created by Admin on 8/11/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class _ViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        @IBAction func next(_ sender: Any){
+//            
+//        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- demoBasicUserControl/AppDelegate.swift
+++ demoBasicUserControl/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  demoBasicUserControl
 //
-//  Created by Admin on 8/11/20.
-//  Copyright © 2020 Admin. All rights reserved.
+//  Created by quannh on 8/11/20.
+//  Copyright © 2020 quannh. All rights reserved.
 //
 
 import UIKit
@@ -11,26 +11,29 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+//        Using navigation controller
+//        let navigationController:UINavigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+//        let rootViewController:UIViewController = storyboard.instantiateViewController(withIdentifier: "root") as UIViewController
+//        navigationController.viewControllers = [rootViewController]
+//        self.window?.rootViewController = navigationController
+//        self.window?.makeKeyAndVisible()
+        
+//        Without navigation controller
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "root")
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
 
 }
